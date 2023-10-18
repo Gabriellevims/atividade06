d22b2cfa031f9cf7cd8b34ca432e65c0d7a1f590
gabriellevims <gabriellevi2104@gmail.com>
2023-10-18 11:10:03 -0300

    tabuada

---
+++ atividade06.py
@@ -0,0 +1,5 @@
+# Digite um numero interio e veja sua tabuada
+numero = int(input("Digite um número:"))
+vezes = 0
+numero01 = 0
+while vezes < 10:vezes+=1;num=+1;print(f"tabuada{numero}:{numero*vezes}")
